ab4b9744fef5ceb8cfdf9d06439ae849c132736e
BianSuma <biansuma21@gmail.com>
2020-09-28 03:07:00 +0700

    Updated files

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 import pandas as pd
+import warnings
 import os
 import sys
 import traceback
 import logging
+import numpy as np
 from weather.forms import WeatherForm
 from weather.models import Weathers
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+warnings.filterwarnings('ignore')
 
 # Create your views here.
 
@@ -29,43 +35,59 @@ def read_file(filename, **kwargs):
     return read_map[ext](filename, **kwargs)
 
 
-def predict(mdl):
-    print('Model')
+def checkNanForDataFeature(y):
+    y.replace('-', np.nan, inplace=True)
+    y.fillna(method="ffill", inplace=True)
+
+
+def checkNanForDataTarget(X):
+    X.replace('-', np.nan, inplace=True)
+    X.fillna(method='ffill', inplace=True)
+
+
+def predict(target):
+    data_cuaca = pd.read_excel('static/Weather Raw Data/Data Ta.xlsx')
+
+    X = data_cuaca.drop(['Tanggal', 'Td_Med_C', 'Wind_Dir', 'Pres_S_Lev_Hp', 'Prec_mm', 'TotClOct', 'LowClOct', 'Sun_D_1_h', 'Vis_Km', 'Daily_Weather_Summary_09_00_00', 'Daily_Weather_Summary_12_00_00',
+                         'Daily_Weather_Summary_15_00_00', 'Daily_Weather_Summary_18_00_00', 'Daily_Weather_Summary_21_00_00', 'Daily_Weather_Summary_00_00_00', 'Daily_Weather_Summary_03_00_00', 'Daily_Weather_Summary_06_00_00'], axis=1)
+    y = data_cuaca['Daily_Weather_Summary_09_00_00']
+
+    checkNanForDataFeature(y)
+    checkNanForDataTarget(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, stratify=y)
+
+    tree_model = DecisionTreeClassifier(max_depth=3)
+
+    tree_fit = tree_model.fit(X_train, y_train)
+
+    param_dct = {
+        'random_state': np.arange(21, 25),
+    }
+
+    gscv = GridSearchCV(tree_fit, param_dct,
+                        scoring='accuracy', return_train_score=True)
+
+    gscv.fit(X_train, y_train)
+
+    return gscv.predict(target)
 
 
 def index(request):
     weather = Weathers.objects.all()
+    last_index = len(weather) - 1
     return render(request, 'views/home.html', {
-        'weather': weather[0]
+        'weather': weather[last_index]
     })
 
 
 def showWeatherDataMaster(request):
     weather_data = read_file('static/Weather Raw Data/Data Ta.xlsx')
-    weathers = {
-        'tanggal': weather_data['Tanggal'],
-        'temp_maks': weather_data['Temperature Max'],
-        'temp_min': weather_data['Temperature Min'],
-        'temp_med': weather_data['Temperature Med'],
-        'td_med': weather_data['Td. Med (C)'],
-        'hr_med': weather_data['Hr. Med (%)'],
-        'wind_dir': weather_data['Wind (km/h) Dir.'],
-        'wind_int': weather_data['Wind (km/h) Int.'],
-        'pres_lev': weather_data['Pres. S. Lev (Hp)'],
-        'prec': weather_data['Prec. (mm)'],
-        'totcloct': weather_data['TotClOct'],
-        'lowcloct': weather_data['LowClOct'],
-        'sun': weather_data['Sun D-1 (h)'],
-        'vis_km': weather_data['Vis Km'],
-        'sum_09': weather_data['Daily Weather Summary 09.00.00'],
-        'sum_12': weather_data['Daily Weather Summary 12.00.00'],
-        'sum_15': weather_data['Daily Weather Summary 15.00.00'],
-        'sum_18': weather_data['Daily Weather Summary 18.00.00'],
-        'sum_21': weather_data['Daily Weather Summary 21.00.00'],
-        'sum_00': weather_data['Daily Weather Summary 00.00.00'],
-        'sum_03': weather_data['Daily Weather Summary 03.00.00'],
-        'sum_06': weather_data['Daily Weather Summary 06.00.00'],
-    }
+    weathers = []
+    for i in range(weather_data.shape[0]):
+        temp = weather_data.iloc[i]
+        weathers.append(dict(temp))
 
     return render(request, 'views/master.html', {
         'weathers': weathers
@@ -82,12 +104,18 @@ def showReport(request):
 def showPredictPage(request):
     if request.method == 'POST':
         data = request.POST
-        print(data)
-        print(sys.stderr)
-        try:
-            weather = Weathers.objects.create()
-            print('Submit has been clicked')
-            return redirect('http://localhost:8000/report')
-        except:
-            print(sys.stderr)
+        target = {
+            'temp_maks': data['tempmaks'],
+            'temp_min': data['tempmin'],
+            'temp_med': data['tempmed'],
+            'hr_med': data['hrmed'],
+            'wind_int': data['windint']
+        }
+        form = WeatherForm(request.POST)
+        print(form)
+        # df_target = pd.DataFrame(data=target, index=[0])
+        # summary = predict(df_target)
+        # print(summary)
+        # form =
+        return redirect('http://localhost:8000/report')
     return render(request, 'views/predict.html')
